f3383f24f1997cc949c60dc3f01f839ba3252af6
Alin Dreghiciu <adreghiciu@gmail.com>
2009-06-29 14:50:10 +0000

    Re-package to org.ops4j.pax.scanner namespace.
    git-svn-id: https://scm.ops4j.org/repos/ops4j/projects/pax/scanner@15593 9b982a3c-3ae5-0310-a4bc-d9a3335569bd

--- pax-scanner-composite/src/main/java/org/ops4j/pax/scanner/composite/ServiceConstants.java
+++ pax-scanner-composite/src/main/java/org/ops4j/pax/scanner/composite/ServiceConstants.java
@@ -15,7 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.ops4j.pax.runner.scanner.composite;
+package org.ops4j.pax.scanner.composite;
 
 /**
  * An enumeration of constants related to composite scanner.
